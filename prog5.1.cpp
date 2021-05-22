9b57edcfeb3e832698f9c71c2d80528cb5036478
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-23 00:01:01 +0530

    Add files via upload

---
+++ prog5.1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	float ratio;
+	printf("Enter four numbers\n");
+	scanf("%d %d %d %d",&a,&b,&c,&d);
+	if(c-d!=0)
+	{
+		ratio=(float)(a+b)/(float)(c-d);
+		printf("Ratio= %f\n", ratio);
+		
+	}
+}
