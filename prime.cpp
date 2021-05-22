3e3d2b2f47875e39a072fbf862ffc2de7c24c79f
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-23 00:04:17 +0530

    Add files via upload

--- prime.cpp
+++ prime.cpp
@@ -2,41 +2,19 @@
 using namespace std;
 int main()
 {
-	int n,i,j,a=1,b=1,c,m,k;
-	cout<<"Enter the value of n""\n";
-	cin>>n;
-	for(k=1;k<=n;k++)
-	{
-		if(k%2==0)
-		{
-		for(int i=1;i<n;i++)
-		{
-			if(i==1 || i==0)
-				continue;
-			int flag=1;
-			for(j=2;j<=i/2;++j)
-			{
-				if(i%j==0)
-				{
-					flag=0;
-					break;
-				}
-			}
-		if(flag==1)
-			cout<<i<<" ";
-		}
-		}
-		else
-		{
-			cout<<a<<" ";
-			for(m=1;m<n/2;m++)
-			{
-				c=a+b;
-				a=b;
-				b=c;
-				cout<<" "<<c;
-			}
-		}
-	}
-	return 0;
+    int n,i,flag=0;
+    cout<<"Enter the number to check prime";
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Not Prime";
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+        cout<<"Prime";
+    return 0;
 }
