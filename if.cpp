248124a4badfa5863dcd3272ea4d7f5b0c2d9b97
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-23 00:03:35 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter the age";
+    cin>>age;
+    if(age>=18)
+        cout<<"Eligible";
+    else
+        cout<<"Not Eligible";
+    return 0;
+}
